111e831634189555d9135835cefbb8f8c6b0566d
dobbi84 <davide.obbi@e4company.com>
2025-12-22 09:23:02 +0100

    fix(ci): resolve ansible-lint and molecule test failures

--- extensions/molecule/testinfra_helpers.py
+++ extensions/molecule/testinfra_helpers.py
@@ -9,7 +9,7 @@ import os
 def get_target_hosts():
     """Return list of target hosts from MOLECULE_INSTANCE_CONFIG."""
     return [
-        "ansible://{}?ansible_inventory={}".format(
+        "ansible://{0}?ansible_inventory={1}".format(
             host, os.environ["MOLECULE_INVENTORY_FILE"]
         )
         for host in os.environ.get("MOLECULE_HOSTS", "all").split(",")
